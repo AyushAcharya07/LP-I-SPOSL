2a51bf04785f6c08389d35b03576a0feaed1b391
Ayush_S_Acharya <115517887+AyushAcharya07@users.noreply.github.com>
2023-10-11 11:46:36 +0530

    Add files via upload

---
+++ Memory_Allocn.cpp
@@ -0,0 +1,245 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	int n, m,ch,ans;
+    cout << "Enter the no. of blocks of memory: " << endl;
+    cin >> n;
+    int blk_size[n];
+    memset(blk_size, 0, sizeof(blk_size));
+
+    cout << "Enter the Memory block sizes: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> blk_size[i];
+    }
+
+    cout << "\nThe Memory blocks are: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Memory Block " << i + 1 << " is : " << blk_size[i] << endl;
+    }
+
+    cout << "\nEnter the no. of processes: " << endl;
+    cin >> m;
+    int process_size[m];
+    bool allocated[n];
+
+    cout << "Enter the process sizes: " << endl;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> process_size[i];
+    }
+
+    cout << "\nThe processes with size are: " << endl;
+    for (int i = 0; i < m; i++)
+    {
+        cout << "Process " << i + 1 << " : Size is : " << process_size[i] << endl;
+    }
+
+    memset(allocated, false, sizeof(allocated));
+
+    int allocation_block[m];
+    string allocation_status[m];
+    cout<<endl;
+    cout<<"\n\t\tMEMORY ALLOCATION ALGO"<<endl;
+    cout<<"\n1.First_Fit\n2.Best_Fit\n3.Worst_Fit\n4.Next_Fit"<<endl;
+    cout<<"\nEnter your choice for using Memory Allocation : "<<endl;
+    cin>>ch;
+    
+    do
+    {
+    	switch(ch)
+    	{
+    		case 1:
+				for (int i = 0; i < m; i++)
+    			{
+					bool allocated_flag = false;
+
+					for (int j = 0; j < n; j++)
+					{
+						if (!allocated[j] && blk_size[j] >= process_size[i])
+						{
+						    allocation_block[i] = j + 1;
+						    allocation_status[i] = "Allocated";
+						    allocated[j] = true;
+						    allocated_flag = true;
+						    break;
+						}
+					}
+
+					
+					if (!allocated_flag)
+					{
+						allocation_block[i] = -1; 
+						allocation_status[i] = "Not Allocated";
+					}
+    			}
+
+				cout << "\nProcess\t\tProcess Size\t\tMemory Block\t\tStatus" << endl;
+				cout<<endl;
+				for (int i = 0; i < m; i++)
+				{
+					cout << "Process " << i + 1 << "\t\t" << process_size[i] << "\t\t";
+					if (allocation_block[i] != -1)
+					{
+						cout <<"\t"<<allocation_block[i] << "\t\t";
+					}
+					else
+					{
+						cout << "----\t\t";
+					}
+					cout<<allocation_status[i] << endl;
+				}
+				break;
+				
+				
+			case 2:
+				for (int i=0;i<m;i++)
+    			{
+					int best_blk=-1;
+					int min=1000000;
+					for (int j=0;j<n;j++)
+					{
+						if (!allocated[j] && blk_size[j]>=process_size[i])
+						{
+						    if (blk_size[j]-process_size[i]<min)
+						    {
+						        best_blk=j;
+						        min=blk_size[j]-process_size[i];
+						    }
+						}
+					}
+
+					if (best_blk!=-1)
+					{
+						allocation_block[i]=best_blk+1;
+						allocation_status[i]="Allocated";
+						allocated[best_blk]=true;
+					}
+					else
+					{
+						allocation_block[i]=-1;
+						allocation_status[i]="Not Allocated";
+					}
+    			}
+
+				cout << "\nProcess\t\tProcess Size\t\tMemory Block\t\tStatus" << endl;
+				cout << endl;
+				for (int i = 0; i < m; i++)
+				{
+					cout << "Process " << i + 1 << "\t\t" << process_size[i] << "\t";
+					if (allocation_block[i] != -1)
+					{
+						cout << "\t" << allocation_block[i] << "\t\t";
+					}
+					else
+					{
+						cout << "----\t\t";
+					}
+					cout << allocation_status[i] << endl;
+				}
+				break;
+				
+			case 3:
+				for (int i=0;i<m;i++)
+    			{
+					int best_blk=-1;
+					int max=-1;
+					for (int j=0;j<n;j++)
+					{
+						if (!allocated[j] && blk_size[j]>=process_size[i])
+						{
+						    if (blk_size[j]-process_size[i]>max)
+						    {
+						        best_blk=j;
+						        max=blk_size[j]-process_size[i];
+						    }
+						}
+					}
+
+					if (best_blk!=-1)
+					{
+						allocation_block[i]=best_blk+1;
+						allocation_status[i]="Allocated";
+						allocated[best_blk]=true;
+					}
+					else
+					{
+						allocation_block[i]=-1;
+						allocation_status[i]="Not Allocated";
+					}
+    			}
+
+				cout << "\nProcess\t\tProcess Size\t\tMemory Block\t\tStatus" << endl;
+				cout << endl;
+				for (int i = 0; i < m; i++)
+				{
+					cout << "Process " << i + 1 << "\t\t" << process_size[i] << "\t";
+					if (allocation_block[i] != -1)
+					{
+						cout << "\t" << allocation_block[i] << "\t";
+					}
+					else
+					{
+						cout << "----\t\t";
+					}
+					cout << allocation_status[i] << endl;
+				}
+				break;
+				
+			case 4:
+				for (int i = 0; i < m; i++)
+    			{
+					bool allocated_flag = false;
+
+					for (int j = 0; j < n; j++)
+					{
+						if (!allocated[j] && blk_size[j] >= process_size[i])
+						{
+						    allocation_block[i] = j + 1;
+						    allocation_status[i] = "Allocated";
+						    allocated[j] = true;
+						    allocated_flag = true;
+						    break;
+						    j++;
+						}
+					}
+
+					
+					if (!allocated_flag)
+					{
+						allocation_block[i] = -1; 
+						allocation_status[i] = "Not Allocated";
+					}
+    			}
+
+				cout << "\nProcess\t\tProcess Size\t\tMemory Block\t\tStatus" << endl;
+				cout<<endl;
+				for (int i = 0; i < m; i++)
+				{
+					cout << "Process " << i + 1 << "\t\t" << process_size[i] << "\t\t";
+					if (allocation_block[i] != -1)
+					{
+						cout <<"\t"<<allocation_block[i] << "\t\t";
+					}
+					else
+					{
+						cout << "----\t\t";
+					}
+					cout<<allocation_status[i] << endl;
+				}
+				break;
+				
+			default:
+				cout<<"\nYou have Entered a Wrong choice!\nPlease Try Again!"<<endl;
+				break;
+
+    	}
+    	cout<<"\nDo you want to continue?\n1.Yes\n2.No"<<endl;
+    	cin>>ans;
+    } while (ans==1);
+}
