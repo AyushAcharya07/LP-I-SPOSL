47fbbd9c6b143234126aa5f290bc6d172a04fe7f
Ayush_S_Acharya <115517887+AyushAcharya07@users.noreply.github.com>
2023-10-22 15:04:34 +0530

    Add files via upload

--- next_fit.cpp
+++ next_fit.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 using namespace std;
 
-int main()
+int main() 
 {
     int n, m;
     cout << "Enter the no. of blocks of memory: " << endl;
@@ -12,74 +12,75 @@ int main()
     memset(blk_size, 0, sizeof(blk_size));
 
     cout << "Enter the Memory block sizes: " << endl;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cin >> blk_size[i];
     }
 
     cout << "\nThe Memory blocks are: " << endl;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cout << "Memory Block " << i + 1 << " is : " << blk_size[i] << endl;
     }
 
-    cout << "\Enter the no. of processes: " << endl;
+    cout << "Enter the no. of processes: " << endl;
     cin >> m;
     int process_size[m];
     bool allocated[n];
-    memset(allocated, false, sizeof(allocated));
 
     cout << "Enter the process sizes: " << endl;
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++) {
         cin >> process_size[i];
     }
 
     cout << "\nThe processes with size are: " << endl;
-    for (int i = 0; i < m; i++)
-    {
-        cout << "Process " << i + 1 << ": Size is : " << process_size[i] << endl;
+    for (int i = 0; i < m; i++) {
+        cout << "Process " << i + 1 << " : Size is : " << process_size[i] << endl;
     }
 
+    memset(allocated, false, sizeof(allocated));
+
+    // Create arrays to store allocation information
     int allocation_block[m];
     string allocation_status[m];
 
-    int j = 0;  // Initialize memory block index
-    for (int i = 0; i < m; i++)
-    {
-        bool allocated_flag = false;
+    int j = 0, t = n - 1;
 
-        while (j < n)
-        {
-            if (!allocated[j] && blk_size[j] >= process_size[i])
-            {
+    for (int i = 0; i < m; i++) {
+        bool allocated_flag = false; // Flag to check if process is allocated
+
+        while (j < n) {
+            if (!allocated[j] && blk_size[j] >= process_size[i]) {
                 allocation_block[i] = j + 1;
                 allocation_status[i] = "Allocated";
                 allocated[j] = true;
                 allocated_flag = true;
+                blk_size[j] -= process_size[i];
+                t = (j - 1) % n; // Set a new endpoint
                 break;
             }
-            j = (j + 1) % n;  // Move to the next block in a circular manner
+
+            if (t == j) {
+                t = (j - 1) % n; // Set a new endpoint
+                break;
+            }
+
+            j = (j + 1) % n; // Move to the next block
         }
 
-        if (!allocated_flag)
-        {
-            allocation_block[i] = -1;
+        // If the process was not allocated, mark it as "Not Allocated"
+        if (!allocated_flag) {
+            allocation_block[i] = -1; // Invalid block number
             allocation_status[i] = "Not Allocated";
         }
     }
 
-    cout << "\nProcess\tProcess Size\tMemory Block\tStatus" << endl;
-    for (int i = 0; i < m; i++)
-    {
-        cout << "Process " << i + 1 << "\t" << process_size[i] << "\t";
-        if (allocation_block[i] != -1)
-        {
-            cout << allocation_block[i] << "\t";
-        }
-        else
-        {
-            cout << "----\t";
+    cout << "\nProcess\t\tProcess Size\t\tMemory Block\t\tStatus" << endl;
+    cout << endl;
+    for (int i = 0; i < m; i++) {
+        cout << "Process " << i + 1 << "\t\t" << process_size[i] << "\t\t";
+        if (allocation_block[i] != -1) {
+            cout << "\t" << allocation_block[i] << "\t\t";
+        } else {
+            cout << "----\t\t";
         }
         cout << allocation_status[i] << endl;
     }
